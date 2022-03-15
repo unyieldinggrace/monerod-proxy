54e5447556c4025ecb718b086e267feb10094616
Nick Watts <nick@cravingreality.net>
2022-03-16 09:38:14 +1000

    Proof-of-Concept passing requests through to monerod.

--- endpoints/monerodproxyhandler.go
+++ endpoints/monerodproxyhandler.go
@@ -1,8 +1,11 @@
 package endpoints
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/carlmjohnson/requests"
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,7 +15,26 @@ type MonerodProxyHandlerResponse struct {
 
 func ConfigureMonerodProxyHandler(e *echo.Echo) {
 	e.GET(":monerodendpoint", func(c echo.Context) error {
-		resp := &MonerodProxyHandlerResponse{MonerodEndpoint: c.Param("monerodendpoint")}
-		return c.JSON(http.StatusOK, resp)
+		fmt.Println("Monerod Endpoint: " + c.Param("monerodendpoint"))
+		// resp := &MonerodProxyHandlerResponse{MonerodEndpoint: c.Param("monerodendpoint")}
+		// return c.JSON(http.StatusOK, resp)
+		resp := loadURL(c.Param("monerodendpoint"))
+		return c.String(http.StatusOK, resp)
 	})
+}
+
+func loadURL(endpoint string) string {
+	var content string
+	err := requests.URL("http://xmrnode.digitalcashtools.com:18081/" + endpoint).
+		ContentType("application/json").
+		ToString(&content).
+		Fetch(context.Background())
+
+	if err != nil {
+		fmt.Println("Error calling backend node:")
+		fmt.Println(err)
+	}
+
+	fmt.Println(content)
+	return content
 }
